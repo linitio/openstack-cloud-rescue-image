bd95760f790ce2aa65d98f891d8e3e4eadfc378d
Kevin Allioli <kevin@linit.io>
2023-01-16 15:19:38 +0100

    Add workflow and config files

---
+++ bin/dynamic-motd/README.md
@@ -0,0 +1,96 @@
+# Dynamic motd
+
+The aim of this project is to give some informations when you log into a server through SSH.
+
+Example:
+
+```
+
+   ___  ___ _ ____   _____ _ __
+  / __|/ _ \ '__\ \ / / _ \ '__|
+  \__ \  __/ |   \ V /  __/ |
+  |___/\___|_|    \_/ \___|_|
+
+
+  Debian GNU/Linux 8.4 (jessie) (kernel 3.16.0-4-amd64)
+
+
+  System information as of Fri Apr 22 10:02:57 2016
+
+  System load:  2.74                 Processes:           167
+  Memory usage: 98%                  Users logged in:     1
+  Swap usage:   31%
+  Disk Usage:
+    Usage of /                       : 37.0% of 19.18GB
+    Usage of /boot                   : 36.6% of 0.11GB
+    Usage of /home                   : 11.1% of 501.60GB
+  Inode Usage:
+    Usage of /                       : 12.7% of 1286144
+    Usage of /boot                   : 1.1% of 31232
+    Usage of /home                   : 0.1% of 33406976
+
+  Logged in users:
+  user       from laptop.example.org        at Fri Apr 22 09:09:09 2016
+
+No mail.
+Last login: Fri Apr 22 09:23:01 2016 from laptop.example.org
+```
+
+**Warning** This is Debian and Debian-related distributions only.
+
+## Dependencies
+
+You need to install some packages:
+
+```
+apt-get install figlet lsb-release python-utmp bc
+```
+
+Optionnally, you can install `needrestart` which is used to show a message if your server need a reboot (main reason (and the only one I know): you have upgraded your kernel).
+If you don't install `needrestart`, it will work, but you won't be warned about the need for a reboot.
+`needrestart` warns you about services that need to be restarted too (but is slower than `checkrestart` for that, see below).
+
+You can optionnally install `debian-goodies` which provides `checkrestart`, which will be used to warn you about services that need to be restarted. Relying on `needrestart` for that is slow (±7 seconds) while `checkrestart` do it faster (less than one second).
+
+**NB:** if you use Debian 11 Bullseye, install `python3-utmp` instead of `python-utmp` and change `python` to `python3` in `update-motd.d/20-system-info` and `update-motd.d/sysinfo.py`:
+
+```
+sed -e "s/python/python3/" -i update-motd.d/20-system-info update-motd.d/sysinfo.py
+```
+
+## Installation
+
+```
+cp -r update-motd.d/ /etc
+rm /etc/motd
+ln -s /var/run/motd /etc/motd
+```
+
+## Disabling for some users
+
+Just create a `/etc/update-motd.d/hushlogin` file containing the names of the users, like:
+
+```
+alice
+bob
+```
+
+## Salt
+
+You will find a working salt formula in `init.sls`.
+
+```
+cd /srv/salt
+git clone https://framagit.org/luc/dynamic-motd.git motd
+salt your_server state.sls motd
+```
+
+## License
+
+GPLv2. Have a look at the [LICENSE file](LICENSE).
+
+## Acknowledments
+
+- Dustin Kirkland, the guy behind the Ubuntu dynamic motd (I took some scripts from Ubuntu and stole inspiration too :D)
+- https://github.com/maxis1718/update-motd.d for the skeleton
+- https://github.com/jnweiger/landscape-sysinfo-mini for the python script (slightly modified)
---
+++ config/cc_set_passwords.py
@@ -0,0 +1,342 @@
+# Copyright (C) 2009-2010 Canonical Ltd.
+# Copyright (C) 2012, 2013 Hewlett-Packard Development Company, L.P.
+#
+# Author: Scott Moser <scott.moser@canonical.com>
+# Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#
+# This file is part of cloud-init. See LICENSE file for license information.
+"""Set Passwords: Set user passwords and enable/disable SSH password auth"""
+
+import re
+from logging import Logger
+from string import ascii_letters, digits
+from textwrap import dedent
+from typing import List
+
+from cloudinit import features
+from cloudinit import log as logging
+from cloudinit import subp, util
+from cloudinit.cloud import Cloud
+from cloudinit.config import Config
+from cloudinit.config.schema import MetaSchema, get_meta_doc
+from cloudinit.distros import ALL_DISTROS, Distro, ug_util
+from cloudinit.settings import PER_INSTANCE
+from cloudinit.ssh_util import update_ssh_config
+
+MODULE_DESCRIPTION = """\
+This module consumes three top-level config keys: ``ssh_pwauth``, ``chpasswd``
+and ``password``.
+
+The ``ssh_pwauth`` config key determines whether or not sshd will be configured
+to accept password authentication.
+
+The ``chpasswd`` config key accepts a dictionary containing either or both of
+``users`` and ``expire``. The ``users`` key is used to assign a password to a
+corresponding pre-existing user. The ``expire`` key is used to set
+whether to expire all user passwords specified by this module,
+such that a password will need to be reset on the user's next login.
+
+.. note::
+    Prior to cloud-init 22.3, the ``expire`` key only applies to plain text
+    (including ``RANDOM``) passwords. Post 22.3, the ``expire`` key applies to
+    both plain text and hashed passwords.
+
+``password`` config key is used to set the default user's password. It is
+ignored if the ``chpasswd`` ``users`` is used. Note: the ``list`` keyword is
+deprecated in favor of ``users``.
+"""
+
+meta: MetaSchema = {
+    "id": "cc_set_passwords",
+    "name": "Set Passwords",
+    "title": "Set user passwords and enable/disable SSH password auth",
+    "description": MODULE_DESCRIPTION,
+    "distros": [ALL_DISTROS],
+    "frequency": PER_INSTANCE,
+    "examples": [
+        dedent(
+            """\
+            # Set a default password that would need to be changed
+            # at first login
+            ssh_pwauth: true
+            password: password1
+            """
+        ),
+        dedent(
+            """\
+            # Disable ssh password authentication
+            # Don't require users to change their passwords on next login
+            # Set the password for user1 to be 'password1' (OS does hashing)
+            # Set the password for user2 to a pre-hashed password
+            # Set the password for user3 to be a randomly generated password,
+            #   which will be written to the system console
+            ssh_pwauth: false
+            chpasswd:
+              expire: false
+              users:
+                - name: user1
+                  password: password1
+                  type: text
+                - name: user2
+                  password: $6$rounds=4096$5DJ8a9WMTEzIo5J4$Yms6imfeBvf3Yfu84mQBerh18l7OR1Wm1BJXZqFSpJ6BVas0AYJqIjP7czkOaAZHZi1kxQ5Y1IhgWN8K9NgxR1
+                - name: user3
+                  type: RANDOM
+            """  # noqa
+        ),
+    ],
+    "activate_by_schema_keys": [],
+}
+
+__doc__ = get_meta_doc(meta)
+
+LOG = logging.getLogger(__name__)
+
+# We are removing certain 'painful' letters/numbers
+PW_SET = "".join([x for x in ascii_letters + digits if x not in "loLOI01"])
+
+
+def get_users_by_type(users_list: list, pw_type: str) -> list:
+    """either password or type: RANDOM is required, user is always required"""
+    return (
+        []
+        if not users_list
+        else [
+            (item["name"], item.get("password", "RANDOM"))
+            for item in users_list
+            if item.get("type", "hash") == pw_type
+        ]
+    )
+
+
+def handle_ssh_pwauth(pw_auth, distro: Distro):
+    """Apply sshd PasswordAuthentication changes.
+
+    @param pw_auth: config setting from 'pw_auth'.
+                    Best given as True, False, or "unchanged".
+    @param distro: an instance of the distro class for the target distribution
+
+    @return: None"""
+    service = distro.get_option("ssh_svcname", "ssh")
+    restart_ssh = True
+    try:
+        distro.manage_service("status", service)
+    except subp.ProcessExecutionError as e:
+        uses_systemd = distro.uses_systemd()
+        if not uses_systemd:
+            LOG.debug(
+                "Writing config 'ssh_pwauth: %s'. SSH service '%s'"
+                " will not be restarted because it is not running or not"
+                " available.",
+                pw_auth,
+                service,
+            )
+            restart_ssh = False
+        elif e.exit_code == 3:
+            # Service is not running. Write ssh config.
+            LOG.debug(
+                "Writing config 'ssh_pwauth: %s'. SSH service '%s'"
+                " will not be restarted because it is stopped.",
+                pw_auth,
+                service,
+            )
+            restart_ssh = False
+        elif e.exit_code == 4:
+            # Service status is unknown
+            LOG.warning(
+                "Ignoring config 'ssh_pwauth: %s'."
+                " SSH service '%s' is not installed.",
+                pw_auth,
+                service,
+            )
+            return
+        else:
+            LOG.warning(
+                "Ignoring config 'ssh_pwauth: %s'."
+                " SSH service '%s' is not available. Error: %s.",
+                pw_auth,
+                service,
+                e,
+            )
+            return
+
+    cfg_name = "PasswordAuthentication"
+
+    if isinstance(pw_auth, str):
+        LOG.warning(
+            "DEPRECATION: The 'ssh_pwauth' config key should be set to "
+            "a boolean value. The string format is deprecated and will be "
+            "removed in a future version of cloud-init."
+        )
+    if util.is_true(pw_auth):
+        cfg_val = "yes"
+    elif util.is_false(pw_auth):
+        cfg_val = "no"
+    else:
+        bmsg = f"Leaving SSH config '{cfg_name}' unchanged."
+        if pw_auth is None or pw_auth.lower() == "unchanged":
+            LOG.debug("%s ssh_pwauth=%s", bmsg, pw_auth)
+        else:
+            LOG.warning("%s Unrecognized value: ssh_pwauth=%s", bmsg, pw_auth)
+        return
+
+    updated = update_ssh_config({cfg_name: cfg_val})
+    if not updated:
+        LOG.debug("No need to restart SSH service, %s not updated.", cfg_name)
+        return
+
+    if restart_ssh:
+        distro.manage_service("restart", service)
+        LOG.debug("Restarted the SSH daemon.")
+    else:
+        LOG.debug("Not restarting SSH service: service is stopped.")
+
+
+def handle(
+    name: str, cfg: Config, cloud: Cloud, log: Logger, args: list
+) -> None:
+    distro: Distro = cloud.distro
+    if args:
+        # if run from command line, and give args, wipe the chpasswd['list']
+        password = args[0]
+        if "chpasswd" in cfg and "list" in cfg["chpasswd"]:
+            del cfg["chpasswd"]["list"]
+    else:
+            expire = True
+    if args:
+        # if run from command line, and give args, wipe the chpasswd['list']
+        password = args[0]
+        if "chpasswd" in cfg and "list" in cfg["chpasswd"]:
+            del cfg["chpasswd"]["list"]
+    else:
+        try:
+            password = cloud.datasource.metadata["admin_pass"]
+            expire = False
+        except Exception:
+            password = util.get_cfg_option_str(cfg, "password", None)
+
+    plist: List = []
+    users_list: List = []
+
+    if "chpasswd" in cfg:
+        chfg = cfg["chpasswd"]
+        users_list = util.get_cfg_option_list(chfg, "users", default=[])
+        if "list" in chfg and chfg["list"]:
+            log.warning(
+                "DEPRECATION: key 'lists' is now deprecated. Use 'users'."
+            )
+            if isinstance(chfg["list"], list):
+                log.debug("Handling input for chpasswd as list.")
+                plist = util.get_cfg_option_list(chfg, "list", plist)
+            else:
+                log.warning(
+                    "DEPRECATION: The chpasswd multiline string format is "
+                    "deprecated and will be removed from a future version of "
+                    "cloud-init. Use the list format instead."
+                )
+                log.debug("Handling input for chpasswd as multiline string.")
+                multiline = util.get_cfg_option_str(chfg, "list")
+                if multiline:
+                    plist = multiline.splitlines()
+
+        expire = util.get_cfg_option_bool(chfg, "expire", expire)
+
+    if not (users_list or plist) and password:
+        (users, _groups) = ug_util.normalize_users_groups(cfg, distro)
+        (user, _user_config) = ug_util.extract_default(users)
+        if user:
+            plist = ["%s:%s" % (user, password)]
+        else:
+            log.warning("No default or defined user to change password for.")
+
+    errors = []
+    if plist or users_list:
+        # This section is for parsing the data that arrives in the form of
+        #   chpasswd:
+        #     users:
+        plist_in = get_users_by_type(users_list, "text")
+        users = [user for user, _ in plist_in]
+        hashed_plist_in = get_users_by_type(users_list, "hash")
+        hashed_users = [user for user, _ in hashed_plist_in]
+        randlist = []
+        for user, _ in get_users_by_type(users_list, "RANDOM"):
+            password = rand_user_password()
+            users.append(user)
+            plist_in.append((user, password))
+            randlist.append(f"{user}:{password}")
+
+        # This for loop is for parsing the data that arrives in the deprecated
+        # form of
+        #   chpasswd:
+        #     list:
+        # N.B. This regex is included in the documentation (i.e. the schema
+        # docstring), so any changes to it should be reflected there.
+        prog = re.compile(r"\$(1|2a|2y|5|6)(\$.+){2}")
+        for line in plist:
+            u, p = line.split(":", 1)
+            if prog.match(p) is not None and ":" not in p:
+                hashed_plist_in.append((u, p))
+                hashed_users.append(u)
+            else:
+                # in this else branch, we potentially change the password
+                # hence, a deviation from .append(line)
+                if p == "R" or p == "RANDOM":
+                    p = rand_user_password()
+                    randlist.append("%s:%s" % (u, p))
+                plist_in.append((u, p))
+                users.append(u)
+        if users:
+            try:
+                log.debug("Changing password for %s:", users)
+                distro.chpasswd(plist_in, hashed=False)
+            except Exception as e:
+                errors.append(e)
+                util.logexc(
+                    log, "Failed to set passwords with chpasswd for %s", users
+                )
+
+        if hashed_users:
+            try:
+                log.debug("Setting hashed password for %s:", hashed_users)
+                distro.chpasswd(hashed_plist_in, hashed=True)
+            except Exception as e:
+                errors.append(e)
+                util.logexc(
+                    log,
+                    "Failed to set hashed passwords with chpasswd for %s",
+                    hashed_users,
+                )
+
+        if len(randlist):
+            blurb = (
+                "Set the following 'random' passwords\n",
+                "\n".join(randlist),
+            )
+            util.multi_log(
+                "%s\n%s\n" % blurb, stderr=False, fallback_to_stdout=False
+            )
+
+        if expire:
+            users_to_expire = users
+            if features.EXPIRE_APPLIES_TO_HASHED_USERS:
+                users_to_expire += hashed_users
+            expired_users = []
+            for u in users_to_expire:
+                try:
+                    distro.expire_passwd(u)
+                    expired_users.append(u)
+                except Exception as e:
+                    errors.append(e)
+                    util.logexc(log, "Failed to set 'expire' for %s", u)
+            if expired_users:
+                log.debug("Expired passwords for: %s users", expired_users)
+
+    handle_ssh_pwauth(cfg.get("ssh_pwauth"), distro)
+
+    if len(errors):
+        log.debug("%s errors occurred, re-raising the last one", len(errors))
+        raise errors[-1]
+
+
+def rand_user_password(pwlen=20):
+    return util.rand_str(pwlen, select_from=PW_SET)
+
